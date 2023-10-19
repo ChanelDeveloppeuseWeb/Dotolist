eff3ad6039e6bb50f60f5227cbc241ad25a2ec16
kazi kuete charnel <chanelkuete2@gmail.com>
2023-10-19 18:09:51 +0100

    wip

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function taskForm(){
+    let tilte = document.getElementById("title").value
+    let description = document.getElementById("description").value
+    let date = document.getElementById("date").value
+}
